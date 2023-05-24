f46f43438fc4e4e3c8cc7a9a33962a562a452356
Andreas FIVEL <afivel@a203-05.iut.univ-paris8.fr>
2023-05-24 11:28:16 +0200

    Tout bon

--- src/main/java/com/pirateswarriors/Main.java
+++ src/main/java/com/pirateswarriors/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
             //scene.getStylesheets().add(getClass().getResource("/vue/towerDefense.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.show();
+            //OKOKOKOKOK
         } catch (Exception e) {
             e.printStackTrace();
         }
