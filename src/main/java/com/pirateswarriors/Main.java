de46c496a6d132f0ff09d9149918b34e67f78d33
a labib <yssq91@gmail.com>
2023-06-08 12:40:22 +0200

    ajout de l'acceuil

--- src/main/java/com/pirateswarriors/Main.java
+++ src/main/java/com/pirateswarriors/Main.java
@@ -1,20 +1,22 @@
 package com.pirateswarriors;
 
 import com.pirateswarriors.controller.Controller;
+import com.pirateswarriors.controller.ControllerViewAcceuil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
         try {
-            BorderPane root = FXMLLoader.load(getClass().getResource("view.fxml"));
+            BorderPane root = FXMLLoader.load(getClass().getResource("viewAcceuil.fxml"));
             Scene scene = new Scene(root);
-            //scene.getStylesheets().add(getClass().getResource("/vue/towerDefense.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception e) {
