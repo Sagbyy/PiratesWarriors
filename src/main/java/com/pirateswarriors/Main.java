37acba94fb404cca49218a581c6df5147702d90a
A2sq91 <yssq91@gmail.com>
2023-06-12 16:32:52 +0200

    attente reglage media

--- src/main/java/com/pirateswarriors/Main.java
+++ src/main/java/com/pirateswarriors/Main.java
@@ -23,21 +23,21 @@ public class Main extends Application {
             BorderPane root = FXMLLoader.load(getClass().getResource("viewAcceuil.fxml"));
             Scene scene = new Scene(root);
 
-//            // Chargement du fichier audio
-//            Media mediaMusic = new Media(getClass().getResource("/com/pirateswarriors/sounds/PiratesWarriorsMusic.mp3").toString());
-//            // Creation du lecteur media
-//            mediaPlayer = new MediaPlayer(mediaMusic);
-//
-//            // Lecture du media au lancement
-//            mediaPlayer.setVolume(0.44); //  disregarding the volume I set
-//            mediaPlayer.play();
-//            // Repetition de la musique lorsqu'elle est finit
-//            mediaPlayer.setOnEndOfMedia(new Runnable() {
-//                @Override
-//                public void run() {
-//                    mediaPlayer.seek(Duration.ZERO);
-//                }
-//            });
+            // Chargement du fichier audio
+            Media mediaMusic = new Media(getClass().getResource("/com/pirateswarriors/sounds/PiratesWarriorsMusic.mp3").toString());
+            // Creation du lecteur media
+            mediaPlayer = new MediaPlayer(mediaMusic);
+
+            // Lecture du media au lancement
+            mediaPlayer.setVolume(0.44); //  disregarding the volume I set
+            mediaPlayer.play();
+            // Repetition de la musique lorsqu'elle est finit
+            mediaPlayer.setOnEndOfMedia(new Runnable() {
+                @Override
+                public void run() {
+                    mediaPlayer.seek(Duration.ZERO);
+                }
+            });
 
             primaryStage.setScene(scene);
             primaryStage.setFullScreen(true);
@@ -47,11 +47,11 @@ public class Main extends Application {
         }
     }
 
-//    @Override
-//    public void stop() {
-//        // Arrêt du lecteur média lorsque l'application se ferme
-//        mediaPlayer.stop();
-//    }
+    @Override
+    public void stop() {
+        // Arrêt du lecteur média lorsque l'application se ferme
+        mediaPlayer.stop();
+    }
 
     public static void main(String[] args) {
         launch(args);
