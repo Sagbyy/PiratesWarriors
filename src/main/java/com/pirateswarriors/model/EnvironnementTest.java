211aa175cead58de93f9ac91cd949d4cf7199bcd
Ayb774 <yssq91@gmail.com>
2023-06-16 14:29:06 +0200

    parametre test

--- src/main/java/com/pirateswarriors/model/EnvironnementTest.java
+++ src/main/java/com/pirateswarriors/model/EnvironnementTest.java
@@ -1,37 +1,37 @@
-package com.pirateswarriors.model;
-
-import com.pirateswarriors.controller.ControllerViewChoixMap;
-import com.pirateswarriors.model.Ennemis.Ennemis;
-import com.pirateswarriors.view.TresorVue;
-import javafx.scene.layout.Pane;
-import org.junit.Test;
-
-import javax.sound.sampled.Port;
-
-import static org.junit.Assert.*;
-
-public class EnvironnementTest {
-
-
-    @Test
-    public void testSelectionMap() {
-        // Create an instance of Environnement
-        Environnement environnement = new Environnement(null, new Pane(), new PorteMonnaie());
-
-        // Test selecting map 1
-        ControllerViewChoixMap.setMap(1);
-        assertEquals("map1.csv", environnement.SelectionMap());
-
-        // Test selecting map 2
-        ControllerViewChoixMap.setMap(2);
-        assertEquals("map2.csv", environnement.SelectionMap());
-
-        // Test selecting map 3
-        ControllerViewChoixMap.setMap(3);
-        assertEquals("map3.csv", environnement.SelectionMap());
-
-        // Test selecting invalid map
-        ControllerViewChoixMap.setMap(4);
-        assertEquals(null, environnement.SelectionMap()); // Or specify an expected value for an invalid map
-    }
-}
+//package com.pirateswarriors.model;
+//
+//import com.pirateswarriors.controller.ControllerViewChoixMap;
+//import com.pirateswarriors.model.Ennemis.Ennemis;
+//import com.pirateswarriors.view.TresorVue;
+//import javafx.scene.layout.Pane;
+//import org.junit.Test;
+//
+//import javax.sound.sampled.Port;
+//
+//import static org.junit.Assert.*;
+//
+//public class EnvironnementTest {
+//
+//
+//    @Test
+//    public void testSelectionMap() {
+//        // Create an instance of Environnement
+//        Environnement environnement = new Environnement(null, new Pane(), new PorteMonnaie());
+//
+//        // Test selecting map 1
+//        ControllerViewChoixMap.setMap(1);
+//        assertEquals("map1.csv", environnement.SelectionMap());
+//
+//        // Test selecting map 2
+//        ControllerViewChoixMap.setMap(2);
+//        assertEquals("map2.csv", environnement.SelectionMap());
+//
+//        // Test selecting map 3
+//        ControllerViewChoixMap.setMap(3);
+//        assertEquals("map3.csv", environnement.SelectionMap());
+//
+//        // Test selecting invalid map
+//        ControllerViewChoixMap.setMap(4);
+//        assertEquals(null, environnement.SelectionMap()); // Or specify an expected value for an invalid map
+//    }
+//}
